18af03acb115e04c36d7b0e04d43844e4d653b01
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-02-07 11:38:21 +0100

    Three Cards

--- exercice1/src/App.js
+++ exercice1/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/base.css';
+import Card from './components/card';
+
+let users = [
+  {
+    name: 'Édouard Jérémie',
+    work: 'CEO',
+    image: './edouard.jpg'
+  },
+  {
+    name: 'Amélie Christelle',
+    work: 'Directeur des recherche',
+    image: './amelie.avif'
+  },
+  {
+    name: 'Gilles Damien',
+    work: 'Directeur commercial',
+    image: './gilles.jpg'
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="team__header">
+        <h2 className="team__title">Notre équipe</h2>
       </header>
+      <section className="team__content">
+        <h1 className="team__heading">Présentation de notre formidable équipe</h1>
+        <p className="team__description"> Découvrez les esprits créatifs qui propulsent Auto Innovation vers de nouveaux
+          sommets dans le monde de la conduite à hydrogène.</p>
+      </section>
+      <ul className="gallery">
+        {users.map((user, index) => (
+          <Card key={index} name={user.name} work={user.work} image={user.image}/>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ exercice1/src/components/card.js
@@ -0,0 +1,19 @@
+'use client';
+import '../css/base.css';
+
+
+export default function Card(user) {
+    
+    return (
+        <div className="gallery__pic">
+            <img src={user.image} alt={user.work} className="gallery__image"/>
+                <div className="gallery__div">
+                    <img src="./bgshapes.svg"/>
+                        <div className="gallery__txt">
+                            <h2>{user.name}</h2>
+                            <h3 className="gallery__work">{user.work}</h3>
+                        </div>
+                </div>
+        </div>
+    )
+}
