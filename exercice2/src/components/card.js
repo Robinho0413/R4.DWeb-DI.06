8dc220aaf1915a50d9c80bc780f0245cfc1f2b3b
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-02-14 15:41:47 +0100

    Final Exo2

--- exercice2/src/components/card.js
+++ exercice2/src/components/card.js
@@ -2,27 +2,43 @@
 import '../css/base.css';
 
 
-function Advantage({name}){
-    return (
-        <li>
-            <span className='advantage__checkmark'>✓</span>
-            <p className='advantage__text'>{name}</p>
-        </li>
-    )
+function Advantage({ name, isChecked }) {
+    if (isChecked) {
+        return (
+            <>
+                <span className='advantage__checkmark'>✓</span>
+                <p className='advantage__text'>{name}</p>
+            </>
+        )
+
+    }
+    else {
+        return (
+            <>
+                <span class="advantage__notcheckmark">✓</span>
+                <p class="advantage__text">{name}</p>
+            </>
+        )
+    }
 }
 
 export default function Card(offer) {
-    
+
     return (
         <ul className='offer-box'>
             <li className='offer-box__header'>
                 <h3 className='offer-box__name'>{offer.name}</h3>
                 <p className='offer-box__price'>{offer.price}</p>
             </li>
-
-            <Advantage name={offer.adv.name}/>
-
-            
+            {offer.adv.map((adv, index) => (
+                <li class="advantage">
+                    <Advantage key={index} name={adv.name} isChecked={adv.isChecked} />
+                </li>
+            ))}
+            <li >
+                <a href={offer.direction} class="header__lien"><button class="header__button btn--hero__nav">{offer.btn}</button>
+                </a>
+            </li>
         </ul>
     )
 }
---
+++ exercice2/src/components/offer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "../css/base.css";
+import Card from './card';
+
+export default function Page(page) {
+
+    return (
+        <div class="offer">
+            <div class="offer-section">
+                <h2 class="offer-section__title">{page.title}</h2>
+                <p class="offer-section__description">{page.description}</p>
+                <div class="box">
+                    {page.offers.map((offer, index) => (
+                    <div key={index}>
+                        <Card name={offer.name} price={offer.price} adv={offer.adv} direction={offer.direction} btn={offer.btn} />
+                    </div>
+                ))}
+                </div>
+                
+            </div>
+        </div>
+
+    );
+}
